1ff503887416020daa2e9925641bbaae2301775e
Taiwo Oriowo <taiwo.oriowo@bison.howard.edu>
2022-11-30 16:28:59 +0000

    Timer corrected

--- timer.c
+++ timer.c
@@ -3,10 +3,13 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <time.h>
+
+clock_t start, end;
+double cpu_time_used;
 
 
 int check = 0;
-int noOfSecs = 0;
 int noOfAlarms = 0;
 void handler(int signum)
 { //signal handler
@@ -18,7 +21,9 @@ void handler(int signum)
 \
 void cancelHandler(int signum)
 { //signal handler
-  printf("\n%d alarm(s) ran for %d second(s) \n",noOfAlarms, noOfSecs);
+	end = clock();
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+  printf("\n%d alarm(s) ran for %f second(s) \n",noOfAlarms, cpu_time_used);
 	exit(1);
 }
 
@@ -26,14 +31,14 @@ int main(int argc, char * argv[])
 {
   signal(SIGALRM,handler); //register handler to handle SIGALRM
 	signal(SIGINT,cancelHandler);
+	start = clock();
   alarm(1); //Schedule a SIGALRM for 1 second
 
   while(1){//busy wait for signal to be delivered
 		if (check == 1){
 			printf("Turing was right!\n");
 			check = 0;
-			alarm(1);
-			noOfSecs++;}
+			alarm(1);}
 	};
   return 0; //never reached
 
